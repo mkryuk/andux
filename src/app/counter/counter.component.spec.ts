bc2a8f1d6656c1416027d0e50f77368da74f6b5e
mkryuk <mkryuk@gmail.com>
2017-11-28 20:20:25 +0200

    simple example with tassign

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CounterComponent } from './counter.component';
 import { NgRedux } from '@angular-redux/store';
 import { rootReducer } from '../store';
+import { INCREMENT, DECREMENT } from './../actions';
 
 
 describe('CounterComponent', () => {
@@ -28,15 +29,15 @@ describe('CounterComponent', () => {
 });
 
 describe('reducers', () => {
-  it('should handle INCREMENT_COUNTER action', () => {
+  it('should handle INCREMENT action', () => {
     let state;
-    state = rootReducer({ counter: 0 }, { type: 'INCREMENT_COUNTER' });
+    state = rootReducer({ counter: 0 }, { type: INCREMENT });
     expect(state).toEqual({ counter: 1 });
   });
 
-  it('should handle DECREMENT_COUNTER action', () => {
+  it('should handle DECREMENT action', () => {
     let state;
-    state = rootReducer({ counter: 0 }, { type: 'DECREMENT_COUNTER' });
+    state = rootReducer({ counter: 0 }, { type: DECREMENT });
     expect(state).toEqual({ counter: -1 });
   });
 });
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,6 @@
 import { IAppState } from './store';
 import { INCREMENT, DECREMENT } from './actions';
+import { tassign } from 'tassign';
 export interface IAppState {
   counter: number;
 }
@@ -11,9 +12,9 @@ export const INITIAL_STATE: IAppState = {
 export function rootReducer(state: IAppState, action): IAppState {
   switch (action.type) {
     case INCREMENT:
-      return Object.assign({}, state, { counter: state.counter + 1 });
+      return tassign(state, { counter: state.counter + 1 });
     case DECREMENT:
-      return Object.assign({}, state, { counter: state.counter - 1 });
+      return tassign(state, { counter: state.counter - 1 });
   }
   return state;
 }
